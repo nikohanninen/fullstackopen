1bf9d179847cdd78cfc224c903bc511fe06876be
Niko <niko.hanninen23@gmail.com>
2025-05-28 19:42:16 +0300

    updated things

--- part1.1/src/App.jsx
+++ part1.1/src/App.jsx
@@ -1,32 +1,26 @@
 import { useState } from "react"
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [ counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+  
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='Plus'/>
-      <Button onClick={setToZero} text='zero'/>
-      <Button onClick={decreaseByOne} text='minus'/>
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div>
     </div>
   )
 }
