671713a82d9d19cafd3c5d8774d39c503c11a0d2
Niko_Hanninen <niko.hanninen23@gmail.com>
2025-05-28 18:22:31 +0300

    first commit

---
+++ part1.1/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='Plus'/>
+      <Button onClick={setToZero} text='zero'/>
+      <Button onClick={decreaseByOne} text='minus'/>
+    </div>
+  )
+}
+
+export default App
