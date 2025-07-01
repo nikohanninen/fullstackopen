cb6f325ec2052c4b31fac3b1fa90e6afd461f8fd
Niko_Hanninen <niko.hanninen23@gmail.com>
2025-07-01 10:16:14 +0300

    added cors

--- osa0/index.js
+++ osa0/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
