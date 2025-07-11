1c2116f66f55e008bcd25e887e03bd8aa557bade
Niko <niko.hanninen23@gmail.com>
2025-07-11 10:30:05 +0300

    'uibuild'

--- osa0/backend/index.js
+++ osa0/backend/index.js
@@ -97,7 +97,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 
       const body = request.body
 
@@ -112,9 +112,11 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
       })
 
-      note.save().then(savedNote => {
+      note.save()
+      .then(savedNote => {
         response.json(savedNote)
       })
+      .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -128,6 +130,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).send({error: error.message})
   }
 
   next(error)
